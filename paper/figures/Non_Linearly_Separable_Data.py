e15f056a263306707e2a126d3011e2d78cc68ca6
Jay <jay0816@outlook.com>
2023-06-10 17:24:03 +0200

    2nd draft

--- paper/figures/Non_Linearly_Separable_Data.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from matplotlib import pyplot as plt
-
-x = [1, 2, 3, 4, 5]
-y = [0, 0, 0, 0, 0]
-c = ["red", "green", "green", "green", "red"]
-plt.scatter(x, y, color=c)
-
-plt.show()
